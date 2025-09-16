91513851e7fb6e3c46acc4f449b3b54baf281303
ManInDark <61268856+ManInDark@users.noreply.github.com>
2025-09-16 16:11:55 +0200

    refactor: moved datastructures to seperate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,5 @@
 import React, { useRef, useState } from 'react'
-
-class Round {
-  points: number;
-  phaseDone: boolean;
-  constructor(points: Round["points"], phaseDone: Round["phaseDone"]) {
-    this.points = points;
-    this.phaseDone = phaseDone;
-  }
-}
-
-class Player {
-  private name: string;
-  private rounds: Round[];
-  private uuid: string;
-  constructor(name: string) {
-    this.name = name;
-    this.rounds = [];
-    this.uuid = crypto.randomUUID();
-  }
-  getName() { return this.name; }
-  getUUID() { return this.uuid; }
-  addRound(points: Round["points"], phaseDone: Round["phaseDone"]) { this.rounds.push(new Round(points, phaseDone)); }
-  getRounds() { return this.rounds; }
-  getRoundResult(i: number) {
-    const round: Round = this.rounds[i];
-    return (
-      <React.Fragment key={`RR-${i}-${this.getUUID()}`}>
-        <td>{round.points}</td>
-        <td><input type="checkbox" checked={round.phaseDone} readOnly={true} /></td>
-      </React.Fragment>
-    )
-  }
-  getPhase() { return 1 + this.rounds.filter(r => r.phaseDone).length }
-  getPhaseDisplay() { return Math.min(10, this.getPhase()); }
-  getAccumulatedPoints() { return this.rounds.reduce((prev, curr) => prev + curr.points, 0) }
-}
+import { Player } from './Datastructures';
 
 function App() {
   const dialogRef = useRef<HTMLDialogElement>(null);
